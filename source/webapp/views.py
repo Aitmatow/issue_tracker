7aab92117b7b40b3ff41e252ddb6f23654ea2e5e
Diiar <aitmarowd@gmail.com>
2019-09-26 15:09:03 +0600

    Этап 2. Добавил главную страницу для Задач

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+from webapp.models import Issue
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['issues'] = Issue.objects.all()
+        return context
