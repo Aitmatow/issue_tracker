539eff8eb82783d6a5e1a219b4f23899c43b8717
Diiar <aitmarowd@gmail.com>
2019-09-26 16:31:15 +0600

    Этап 2 завершен. Добавил страницу для удаления задачи.

--- source/main/urls.py
+++ source/main/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import IndexView, IssueView, IssueCreate, IssueUpdate
+from webapp.views import IndexView, IssueView, IssueCreate, IssueUpdate, IssueDelete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('issue/<int:pk>', IssueView.as_view(), name = 'issue_view'),
     path('issue/add', IssueCreate.as_view(), name = 'add_issue'),
-    path('issue/update/<int:pk>', IssueUpdate.as_view(), name = 'issue_update')
+    path('issue/update/<int:pk>', IssueUpdate.as_view(), name = 'issue_update'),
+    path('issue/delete/<int:pk>', IssueDelete.as_view(), name = 'issue_delete'),
 ]
