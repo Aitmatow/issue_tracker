3eaa0076fb65bd721dc128c3d54b8e19b7812a77
Diiar <aitmarowd@gmail.com>
2019-10-25 13:28:43 +0600

    Бонус. Сделал переход на нажатую неавторизованным пользователем страницу при авторизации.

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,17 +1,21 @@
+from urllib.parse import urlencode
+
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
-
-# Create your views here.
+NEXT_URL = ''
 def login_view(request):
     context = {}
+    if request.method == 'GET':
+        global NEXT_URL
+        NEXT_URL = request.GET.get('next')
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('issue_list')
+            return redirect('http://localhost:8000' + NEXT_URL)
         else:
             context['has_error'] = True
     return render(request, 'login.html', context=context)
